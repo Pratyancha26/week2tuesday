ec43cc7f21fb076e2543215f153a5c30ee426b1e
Pratyancha26 <patiprat@sheridancollege.ca>
2020-06-02 19:12:59 -0400

    Second commit

--- src/week2tuesday/Week2Tuesday.java
+++ src/week2tuesday/Week2Tuesday.java
@@ -16,6 +16,7 @@ public class Week2Tuesday {
      */
     public static void main(String[] args) {
         System.out.println("Learn Github");
+        System.out.println("New commit");
     }
     
 }
